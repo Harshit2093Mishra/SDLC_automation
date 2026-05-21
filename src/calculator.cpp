745c932b90d78d0ac5f138bc2804706339596e88
Harshit2093Mishra <112201209+Harshit2093Mishra@users.noreply.github.com>
2026-05-21 10:47:43 +0000

    Changes

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -38,7 +38,7 @@ namespace example
 
     int Calculator::minEatingSpeed(std::vector<int> &piles, int h)
     {
-        int low = 1;
+        int low = 0;
         int high = *std::max_element(piles.begin(), piles.end());
         int ans = high;
 
